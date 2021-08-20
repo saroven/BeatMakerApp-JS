68dcb98562efce527ae03999781b73e198d810f0
= <=>
2021-08-20 18:39:13 +0600

    play and stop the audio loop

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ class DrumKit {
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
     this.bpm = 150;
+    this.isPlaying = null;
   }
   activePad() {
     this.classList.toggle("active");
@@ -38,9 +39,25 @@ class DrumKit {
   }
   start() {
     const interval = (60 / this.bpm) * 1000;
-    setInterval(() => {
-      this.repeate();
-    }, interval);
+    //check if playing
+    if (!this.isPlaying) {
+      this.isPlaying = setInterval(() => {
+        this.repeate();
+      }, interval);
+    } else {
+      //clear interval
+      clearInterval(this.isPlaying);
+      this.isPlaying = null;
+    }
+  }
+  updateBtn() {
+    if (!this.isPlaying) {
+      this.playBtn.innerText = "Stop";
+      this.playBtn.classList.add("active");
+    } else {
+      this.playBtn.innerText = "Play";
+      this.playBtn.classList.remove("active");
+    }
   }
 }
 
@@ -54,5 +71,6 @@ drumKit.pads.forEach((pad) => {
 });
 
 drumKit.playBtn.addEventListener("click", function () {
+  drumKit.updateBtn();
   drumKit.start();
 });
